ad771eafe774c8c8300e77c3360a31d7c1da85fa
Sandeep <sandeepsandeepkannan16@gmail.com>
2025-12-29 13:09:51 +0530

    seperated routes with router

--- apps/backend/src/middleware/user.ts
+++ apps/backend/src/middleware/user.ts
@@ -1,7 +1,36 @@
 import  express  from "express";
+import  jwt  from "jsonwebtoken";
+import { Request,Response,NextFunction } from "express";
+
+const secretpassword="hjbvkjjber1242323"
+
 
 const app=express()
 app.use(express.json())
 
-app.use(req,res(){})
+export  function Usermiddleware(req:Request,res:Response,next:NextFunction){
+   const authHeader=req.headers.authorization
+    if (!authHeader) {
+        return res.status(401).json({ message: "No token provided" });
+        }
+    
+   const token = authHeader.split(" ")[1];
+   
+   try{
+        const user=jwt.verify(token!,secretpassword) as any
+            
+            (req as any).user={  username:user.username,
+                    password:user.password,
+                    email :user.email,}
+                
+            next()
+
+            }
+     catch(err){
+           return res.status(401).json({ message: "Invalid token" });
+     }       
+
+    
+
+}
 
